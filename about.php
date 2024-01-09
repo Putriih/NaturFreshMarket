65ad58ea96e4f766a97af1ced5b3db6a7f1b32ab
Putriih <156142579+Putriih@users.noreply.github.com>
2024-01-10 06:24:47 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #000000"><b>Tentang Kami</b></h2>
+	<p class="text-justify"><b>NaturFreshMarket adalah toko online yang menjual Sayur organik, Sayur organik adalah sayur yang dibudidayakan tanpa memakai bahan kimia apa pun, baik ketika pengerjaan pemupukan ataupun ketika penyemprotan hama..</b></p>
+<p><b>
+NaturFreshMarket  adalah salah satu pelopor yang menjual sayuran organik.Produk kami sehat, dan bergizi. Dan 
+memiliki kelebihan Pada sayur dan buah-buahan, tanaman organik mampu menghasilkan hasil pertanian yang lebih segar, sehat, dan tahan lama. Pada beberapa kasus, sayur dan buah yang dihasilkan dari pertanian organik bahkan memiliki ukuran dan bobot yang lebih baik.</b></p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ admin/footer.php
@@ -0,0 +1,5 @@
+<footer style="background-color: #99C68E;padding: 1px;" class="print">
+	<h5 class="text-center"><font color="white">Copyright&copy; NaturFreshMarket</font></h5>
+</footer>
+</body>
+</html>
